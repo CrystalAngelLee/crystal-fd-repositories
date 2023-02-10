55462a288d04c2fb850bb5f5031f7ab2ffa4313f
liyuan97 <liyuan97@jd.com>
2023-02-10 11:23:41 +0800

    feat: system基本使用

--- micro/README.md
+++ micro/README.md
@@ -1,17 +1,15 @@
 # Demo 说明
 
-当前项目中演示样例完整文档说明详见： [微前端](https://github.com/CrystalAngelLee/crystal-fd-notes/blob/main/%E5%BE%AE%E5%89%8D%E7%AB%AF/%E5%BE%AE%E5%89%8D%E7%AB%AF.md)
-
 项目指引
 
 - system 演示样例
-  - [crystal-systemjs](./crystal-systemjs)
+  - [systemjs](./systemjs/)
 - single-spa 演示样例
   - [container](./container)
   - [noframe](./noframe)
   - [reactframe](./reactframe)
   - [vueframe](./vueframe)
-  - [navbar](./navbar)
+  - [navbar](./navbarxxxxx)
   - [tools](./tools)
 - 模块联邦演示样例
   - [container](./fedcontainer)
---
+++ micro/systemjs/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js', // 打包后的文件名称
+    path: path.join(__dirname, 'build'),
+    // !!! 标记将 es module 转换成 system module
+    libraryTarget: 'system',
+  },
+  devtool: 'source-map',
+  devServer: {
+    port: 9000, // 运行端口
+    contentBase: path.join(__dirname, 'build'),
+    historyApiFallback: true,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      // !!! 阻止 JS 文件的引入
+      inject: false,
+    }),
+  ],
+  // !!! 排除不需要打包的模块
+  externals: ['react', 'react-dom', 'react-router-dom'],
+}
